b0a1308242db7376bf526d4a74213ae0b84b96af
algfwq <3104374883@qq.com>
2023-05-01 19:01:10 +0800

    celery异步任务，eventlet非常重要

--- websocket_django/celery_config.py
+++ websocket_django/celery_config.py
@@ -1,14 +1,13 @@
 import os
 from celery import Celery
 
-#eventlet非常重要，Windows特有
-#celery -A websocket_django  worker -l DEBUG -P eventlet
+# eventlet非常重要，Windows特有
+# celery -A websocket_django  worker -l DEBUG -P eventlet
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','websocket_django.settings')  # 设置django环境
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'websocket_django.settings')  # 设置django环境
 
 app = Celery('websocket_django')  # 实例化一个app对象
 
 app.config_from_object('django.conf:settings', namespace='CELERY')  # 使用CELERY_ 作为前缀，在settings中写配置
 
 app.autodiscover_tasks()  # 发现任务文件每个app下的tasks.py文件
-
