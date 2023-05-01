d64a037800065db5db51b43c84707c7933f368f8
algfwq <3104374883@qq.com>
2023-05-01 19:03:45 +0800

    celery异步任务，eventlet非常重要

--- websocket_django/celery_config.py
+++ websocket_django/celery_config.py
@@ -11,3 +11,4 @@ app = Celery('websocket_django')  # 实例化一个app对象
 app.config_from_object('django.conf:settings', namespace='CELERY')  # 使用CELERY_ 作为前缀，在settings中写配置
 
 app.autodiscover_tasks()  # 发现任务文件每个app下的tasks.py文件
+
